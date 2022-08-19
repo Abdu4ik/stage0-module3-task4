f196643b45dfd2c2402cdf6c5b1032e57561d066
Abdu4ik <107094143+Abdu4ik@users.noreply.github.com>
2022-08-20 01:53:40 +0500

    Update IntRepresentation.java

--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,12 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+
+        char c = 99, h = 104, a = 97, r = 114;
+
+        System.out.print(c);
+        System.out.print(h);
+        System.out.print(a);
+        System.out.print(r);
     }
 }
